ff9aa28600a35eb4f46a4a54d3e095a7c8bcc3d3
BorisKnyazev <8e8all@gmail.com>
2016-07-03 23:27:44 +0300

    completed draft

--- cs231n_module1_nn_spiral.m
+++ cs231n_module1_nn_spiral.m
@@ -1,6 +1,6 @@
 % Stanford CS class CS231n: Convolutional Neural Networks for Visual Recognition. 
 % Module 1
-% http://cs231n.github.io/neural-networks-case-study/
+% Matlab version of scripts from http://cs231n.github.io/neural-networks-case-study/
 
 %% Generating some data
 N = 100;
@@ -10,15 +10,13 @@ X = zeros(N*K,D);
 y = zeros(N*K,1);
 for j=1:K
     ix = N*(j-1)+1:N*j;
-    r = linspace(0,1,N);
-    t = linspace((j-1)*4+1,j*4,N)+randn(1,N).*0.2;
+    r = linspace(0,1,N); % radius
+    t = linspace((j-1)*4+1,j*4,N)+randn(1,N).*0.2; % theta
     X(ix,:) = [r.*sin(t);r.*cos(t)]';
     y(ix) = j;
 end
-scatter(X(:,1),X(:,2),4,...
-    cat(1,bsxfun(@plus,zeros(N,3),[1,0,0]),...
-    bsxfun(@plus,zeros(N,3),[0.5,0.5,0]),...
-    bsxfun(@plus,zeros(N,3),[0,0,1])))
+scatter(X(:,1), X(:,2), 40, y);
+colormap jet
 title('The toy spiral data with three classes')
 
 %% Initialize the parameters
@@ -69,7 +67,6 @@ W = 0.01 .* randn(D,K);
 b = zeros(1,K);
 
 % gradient descent loop
-
 for i=1:200
   
   % evaluate class scores, [N x K]
@@ -78,7 +75,6 @@ for i=1:200
   % compute the class probabilities
   exp_scores = exp(scores);
   probs = bsxfun(@times, exp_scores, 1./sum(exp_scores,2));
-  
   
   % compute the loss: average cross-entropy loss and regularization
   corect_logprobs = -log(probs(sub2ind(size(probs),[1:num_examples]',y)));
@@ -104,10 +100,107 @@ for i=1:200
   % perform a parameter update
   W = W - step_size * dW;
   b = b - step_size * db;
- 
 end
 
 % evaluate training set accuracy
 scores = bsxfun(@plus, X * W, b);
 [~,predicted_class] = max(scores, [], 2);
 fprintf('training accuracy: %.2f \n', mean(predicted_class == y))
+
+%% visualize decision boundaries
+[w1,w2] = meshgrid(-1.5:0.01:1.5,-1.5:0.01:1.5);
+scores = bsxfun(@plus, [w1(:),w2(:)] * W, b);
+exp_scores = exp(scores);
+probs = bsxfun(@times, exp_scores, 1./sum(exp_scores,2));
+corect_logprobs = -log(probs(sub2ind(size(probs),[1:num_examples]',y)));
+data_loss = sum(corect_logprobs)/num_examples;
+im = zeros([size(w1),3]);
+im(:,:,3) = reshape(probs(:,1),size(w1));
+im(:,:,2) = reshape(probs(:,2),size(w1));
+im(:,:,1) = reshape(probs(:,3),size(w1));
+imagesc(w1(:),w2(:),im)
+hold on
+scatter(X(:,1),X(:,2), 40, y)
+title('The toy spiral data with three classes')
+
+%% Training a Neural Network
+% initialize parameters randomly
+h = 100; % size of hidden layer
+W = 0.01 .* randn(D,h);
+b = zeros(1,h);
+W2 = 0.01 .* randn(h,K);
+b2 = zeros(1,K);
+
+for i=1:10000
+  
+  % evaluate class scores, [N x K]
+  % evaluate class scores with a 2-layer Neural Network
+  hidden_layer = max(0, bsxfun(@plus, X * W, b)); % note, ReLU activation
+  scores = bsxfun(@plus, hidden_layer * W2, b2);
+
+  % compute the class probabilities
+  exp_scores = exp(scores);
+  probs = bsxfun(@times, exp_scores, 1./sum(exp_scores,2)); % [N x K]
+  
+  % compute the loss: average cross-entropy loss and regularization
+  corect_logprobs = -log(probs(sub2ind(size(probs),[1:num_examples]',y)));
+  data_loss = sum(corect_logprobs)/num_examples;
+  reg_loss = 0.5.*reg*sum(sum(W.*W)) + 0.5.*reg*sum(sum(W2.*W2));
+  loss = data_loss + reg_loss;
+  if mod(i,1000) == 0
+    fprintf('iteration %d: loss %f \n', i, loss)
+  end
+  
+  % compute the gradient on scores
+  dscores = probs;
+  ids = sub2ind(size(probs),[1:num_examples]',y);
+  dscores(ids) = dscores(ids) - 1;
+  dscores = dscores./num_examples;
+  
+  % backpropate the gradient to the parameters
+  % first backprop into parameters W2 and b2
+  % backpropate the gradient to the parameters
+  % first backprop into parameters W2 and b2
+  dW2 = hidden_layer' * dscores;
+  db2 = sum(dscores, 1);
+  
+  % next backprop into hidden layer
+  dhidden = dscores * W2';
+  % backprop the ReLU non-linearity
+  dhidden(hidden_layer <= 0) = 0;
+  % finally into W,b
+  dW = X' * dhidden;
+  db = sum(dhidden, 1);
+  
+  % add regularization gradient contribution
+  dW2 = dW2 + reg .* W2;
+  dW = dW + reg .* W;
+  
+  % perform a parameter update
+  W = W - step_size .* dW;
+  b = b - step_size .* db;
+  W2 = W2 - step_size .* dW2;
+  b2 = b2 - step_size .* db2;
+end
+% evaluate training set accuracy
+hidden_layer = max(0, bsxfun(@plus, X * W, b));
+scores = bsxfun(@plus, hidden_layer * W2, b2);
+[~,predicted_class] = max(scores, [], 2);
+fprintf('training accuracy: %.2f \n', mean(predicted_class == y))
+
+%% visualize decision boundaries
+[w1,w2] = meshgrid(-1.5:0.01:1.5,-1.5:0.01:1.5);
+hidden_layer = max(0, bsxfun(@plus, [w1(:),w2(:)] * W, b));
+scores = bsxfun(@plus, hidden_layer * W2, b2);
+exp_scores = exp(scores);
+probs = bsxfun(@times, exp_scores, 1./sum(exp_scores,2));
+corect_logprobs = -log(probs(sub2ind(size(probs),[1:num_examples]',y)));
+data_loss = sum(corect_logprobs)/num_examples;
+im = zeros([size(w1),3]);
+im(:,:,3) = reshape(probs(:,1),size(w1));
+im(:,:,2) = reshape(probs(:,2),size(w1));
+im(:,:,1) = reshape(probs(:,3),size(w1));
+imagesc(w1(:),w2(:),im)
+hold on
+scatter(X(:,1),X(:,2), 40, y)
+title('The toy spiral data with three classes')
